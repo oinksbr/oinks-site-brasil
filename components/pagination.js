caa726754a8fe75650cd9ed7052802bb4b0402de
Felipe Carasco <felipe@carasco.com.br>
2023-07-07 01:15:46 +0100

    🩹 fix: change rule to load total pages

--- components/pagination.js
+++ components/pagination.js
@@ -1,93 +1,95 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Pagination({currentPage, totalPages}) {
-  const [currentPageButton, setCurrentPageButton] = useState(1);
-  const limit = parseInt(process.env.PAGINATION_DISPLAY_PAGES);
+function Pagination({ currentPage, totalPages }) {
+    const [currentPageButton, setCurrentPageButton] = useState(1);
+    const limit = parseInt(process.env.PAGINATION_DISPLAY_PAGES);
+    const [pages, setPages] = useState([]);
+    
+    useEffect(() => {
+        setPages(Array.from({ length: totalPages <= limit ? totalPages : limit }, (_, i) => i + 1))
+    }, [totalPages]);
 
+    useEffect(() => {
+        setVisiblePages();
+    }, [currentPageButton]);
 
-  const [pages, setPages] = useState(Array.from({length: totalPages <= limit ? totalPages : limit}, (_, i) => i + 1));
-  console.log(pages);
-  useEffect(() => {
-    setVisiblePages();
-  }, [currentPageButton]);
-
-  function setVisiblePages() {       
-    const currentPages = [...pages];    
-    if (totalPages > limit) { 
-        if (pages.indexOf(currentPageButton) === limit-1) {       
-            if (currentPageButton < totalPages) { 
-                currentPages.splice(0, 1);
-                currentPages.push(currentPageButton+1)
-            }   
-        }
-        if (currentPages.indexOf(currentPageButton) === 0) {
-            if (currentPageButton > 1) { 
-                currentPages.splice(currentPages.length-1, 1);
-                currentPages.splice(0, 0, currentPageButton-1)
-            }   
-        }
-        setPages(currentPages)
-    }
-}
-
-  function setCurrentPage(number) {
-    currentPage(number);
-    setCurrentPageButton(number);
-  }
-  return (
-    <>
-        {(() => {
-            if (totalPages > 1) {
-                return (
-                    <nav className="navbar justify-content-center mt-5">
-                        <ul className="pagination">
-                            {(() => {
-                            if (currentPageButton !== 1){
-                                return (
-                                    <li className={`page-item ${currentPageButton === 1 ? "disabled" : ""}`}>
-                                        <a className="page-link" href="#" onClick={() => setCurrentPage(currentPageButton-1)}>
-                                            Anterior
-                                        </a>
-                                    </li>
-                                )
-                            }                            
-                            return null;
-                            })()}
-
-                            {                
-                                pages.map((number) => {                   
-                                    return (
-                                        <li className={`page-item ${currentPageButton === number ? "active" : ""}`}>
-                                            <a className="page-link" href="#" onClick={() => setCurrentPage(number)}>
-                                                {number}
-                                            </a>
-                                        </li>
-                                    )
-                                })
-                            }      
-              
-                            {(() => {
-                            if (currentPageButton !== totalPages){
-                                return (
-                                    <li className={`page-item ${currentPageButton === totalPages ? "disabled" : ""}`}>
-                                        <a className="page-link" href="#" onClick={() => setCurrentPage(currentPageButton+1)}>
-                                            Próximo
-                                        </a>
-                                    </li>
-                                )
-                            }
-                            return null;
-                            })()}        
-                
-                        </ul>
-                    </nav>   
-                );
+    function setVisiblePages() {
+        const currentPages = [...pages];
+        if (totalPages > limit) {
+            if (pages.indexOf(currentPageButton) === limit - 1) {
+                if (currentPageButton < totalPages) {
+                    currentPages.splice(0, 1);
+                    currentPages.push(currentPageButton + 1)
+                }
             }
-            return null;
+            if (currentPages.indexOf(currentPageButton) === 0) {
+                if (currentPageButton > 1) {
+                    currentPages.splice(currentPages.length - 1, 1);
+                    currentPages.splice(0, 0, currentPageButton - 1)
+                }
+            }
+            setPages(currentPages);
+        }
+    }
+
+    function setCurrentPage(number) {
+        currentPage(number);
+        setCurrentPageButton(number);
+    }
+    return (
+        <>
+            {(() => {
+                if (totalPages > 1) {
+                    return (
+                        <nav className="navbar justify-content-center mt-5">
+                            <ul className="pagination">
+                                {(() => {
+                                    if (currentPageButton !== 1) {
+                                        return (
+                                            <li className={`page-item ${currentPageButton === 1 ? "disabled" : ""}`}>
+                                                <a className="page-link" href="#" onClick={() => setCurrentPage(currentPageButton - 1)}>
+                                                    Anterior
+                                                </a>
+                                            </li>
+                                        )
+                                    }
+                                    return null;
+                                })()}
+
+                                {
+                                    pages.map((number) => {
+                                        return (
+                                            <li className={`page-item ${currentPageButton === number ? "active" : ""}`}>
+                                                <a className="page-link" href="#" onClick={() => setCurrentPage(number)}>
+                                                    {number}
+                                                </a>
+                                            </li>
+                                        )
+                                    })
+                                }
+
+                                {(() => {
+                                    if (currentPageButton !== totalPages) {
+                                        return (
+                                            <li className={`page-item ${currentPageButton === totalPages ? "disabled" : ""}`}>
+                                                <a className="page-link" href="#" onClick={() => setCurrentPage(currentPageButton + 1)}>
+                                                    Próximo
+                                                </a>
+                                            </li>
+                                        )
+                                    }
+                                    return null;
+                                })()}
+
+                            </ul>
+                        </nav>
+                    );
+                }
+                return null;
             })()}
-    </>
-  );
+        </>
+    );
 }
 
 export default Pagination;
--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,47 @@ import ProductListBar from "../components/product/product-list-bar";
 import Pagination from "../components/pagination";
 
 function Products() {
-  const [products, setProducts] = useState([]);
-  const [sortProducts, setSortProducts] = useState("-discount");
-  const [page, setPage] = useState(1)
-  const [loading, setLoading] = useState(false);
-  const propagandas = [{ "id": 1, "name": "amazon", "index": 3, "image_url": "https://i.promobit.com.br/185761646816879928968906621814_banner.png", "type": "grid" }, { "id": 2, "name": "amazon", "index": 1, "image_url": "https://ww1.nam.ac.uk/wp-content/uploads/2014/07/1015646.jpg", "type": "sidebar" }, { "id": 2, "name": "amazon", "index": 2, "image_url": "https://pt-br-media-publications.shopfully.cloud/publications/page_assets/419534/2/page_2_level_2_1555278997.jpeg", "type": "sidebar" }]
+    const [products, setProducts] = useState([]);
+    const [sortProducts, setSortProducts] = useState("-discount");
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false);
+    const propagandas = [{ "id": 1, "name": "amazon", "index": 3, "image_url": "https://i.promobit.com.br/185761646816879928968906621814_banner.png", "type": "grid" }, { "id": 2, "name": "amazon", "index": 1, "image_url": "https://ww1.nam.ac.uk/wp-content/uploads/2014/07/1015646.jpg", "type": "sidebar" }, { "id": 2, "name": "amazon", "index": 2, "image_url": "https://pt-br-media-publications.shopfully.cloud/publications/page_assets/419534/2/page_2_level_2_1555278997.jpeg", "type": "sidebar" }]
+  
+    useEffect(() => {
+        fetchProducts();
+    }, [page, sortProducts]);
 
-  useEffect(() => {
-    fetchProducts();    
-  }, [page, sortProducts]);
-
-  const fetchProducts = async () => {
-    setLoading(true);
-    const params = `page=${page}&size=50&sort_by=${encodeURIComponent(sortProducts)}`;
-    await fetch(`${process.env.API_BASE_URL}/deal/?${params}`)
-        .then((response) => response.json())
-        .then((data) => {
-            setProducts(data)
-        })
+    const fetchProducts = async () => {
+        setLoading(true);
+        const params = `page=${page}&size=50&sort_by=${encodeURIComponent(sortProducts)}`;
+        await fetch(`${process.env.API_BASE_URL}/deal/?${params}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data)
+            })
         setLoading(false);
-  }
+    }
 
-  return (
-    <div className="vstack">
-        <div className="container py-4">
-            <div className="row g-3">            
-                <div className="col-lg-9"> 
-                    {
-                      loading ? <div className="hstack top-bar justify-content-between mb-3">Carregando...</div> :     
-                      <>
-                        <ProductListBar totalItems={products.totalItems} sortDeals={setSortProducts} />
-                        <ProductGrid products={products} /></>
-                    }
+    return (
+        <div className="vstack">
+            <div className="container py-4">
+                <div className="row g-3">
+                    <div className="col-lg-9">
+                        {
+                            loading ? <div className="hstack top-bar justify-content-between mb-3">Carregando...</div> :
+                                <>
+                                    <ProductListBar totalItems={products.totalItems} sortDeals={setSortProducts} />
+                                    <ProductGrid products={products} /></>
+                        }
+                    </div>
+                    <div className="col-lg-3 vstack gap-2">
+                        <Sidebar advertising={propagandas} />
+                    </div>
+                    <Pagination currentPage={setPage} totalPages={products.totalPages} />
                 </div>
-                <div className="col-lg-3 vstack gap-2">
-                    <Sidebar advertising={propagandas} />
-                </div>
-                <Pagination currentPage={setPage} totalPages={products.totalPages} />
             </div>
         </div>
-    </div>
-);
+    );
 }
 
 export default Products;
